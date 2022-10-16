44b4aff1172d002c8e974985894bf3d477664af1
Angel04172002 <58862906+Angel04172002@users.noreply.github.com>
2022-10-16 14:51:46 +0300

    Create renovator.cs

---
+++ Exam preparation/renovators/renovator.cs
@@ -0,0 +1,38 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Renovators
+{
+    public class Renovator
+    {
+        public Renovator(string name, string type, double rate, int days)
+        {
+            Name = name;
+            Type = type;
+            Rate = rate;
+            Days = days;
+        }
+
+        public string Name { get; set; }
+        public string Type { get; set; }
+        public double Rate { get; set; }
+        public int Days { get; set; }
+
+        public bool Hired { get; set; } = false;
+
+   
+        public override string ToString()
+        {
+            StringBuilder sb = new StringBuilder();
+            sb.AppendLine($"-Renovator: {this.Name}");
+            sb.AppendLine($"--Specialty: {this.Type}");
+            sb.AppendLine($"--Rate per day: {this.Rate} BGN"); 
+
+            return sb.ToString();
+        }
+           
+    }
+}
