5f6ea34fc3b822f64ef55b81b6a8e63101050d46
Angel04172002 <58862906+Angel04172002@users.noreply.github.com>
2022-12-26 23:45:01 +0200

    Create sumNumbers.cs

---
+++ Functional programming/sumNumbers.cs
@@ -0,0 +1,15 @@
+using System;
+using System.Linq;
+
+namespace SumNumbers
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] numbers = Console.ReadLine().Split(", ").Select(int.Parse).ToArray();
+            Console.WriteLine(numbers.Length);
+            Console.WriteLine(numbers.Sum());
+        }
+    }
+}
