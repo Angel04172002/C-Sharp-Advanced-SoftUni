c9d6ebb3f6726762b150cb1271937149cf0fab8d
Angel04172002 <58862906+Angel04172002@users.noreply.github.com>
2022-12-29 01:16:45 +0200

    Create StartUp.cs

---
+++ Defining Classes/StartUp.cs
@@ -0,0 +1,16 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+
+namespace CarManufacturer
+{
+    public class StartUp
+    {
+        static void Main(string[] args)
+        {
+ 
+        }
+    }
+}
