c65b527df9c35b0d21ab7762aa95dec1e0c2022e
Angel04172002 <58862906+Angel04172002@users.noreply.github.com>
2022-12-30 13:37:42 +0200

    Create Cargo.cs

---
+++ Defining Classes - exercise/Raw Data/Cargo.cs
@@ -0,0 +1,13 @@
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace RawData
+{
+    public class Cargo
+    {
+        public string Type { get; set; }
+        public int Weight { get; set; }
+
+    }
+}
