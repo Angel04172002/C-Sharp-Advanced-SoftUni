31bd1b52a82d9111b9b1086e0d52350f124efd01
Angel04172002 <58862906+Angel04172002@users.noreply.github.com>
2022-12-30 13:39:07 +0200

    Create Tire.cs

---
+++ Defining Classes - exercise/Raw Data/Tire.cs
@@ -0,0 +1,13 @@
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace RawData
+{
+    public class Tire
+    {
+        public int Age { get; set; }
+
+        public double Pressure { get; set; }
+    }
+}
